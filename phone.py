035aa6001b1c6572b029970d9a5e93be8ba5fc41
Roman Kozlovets <romakozlovets@gmail.com>
2025-04-28 13:56:06 +0200

    Create phone.py

---
+++ phone.py
@@ -0,0 +1,16 @@
+my_phone = {}
+
+key_name1 = input('Enter the name of first key... ')
+value_name1 = input('Enter the value of the first key... ')
+
+key_name2 = input('Enter the name of second key... ')
+value_name2 = input('Enter the value of the second key... ')
+
+key_name3 = input('Enter the name of third key... ')
+value_name3 = input('Enter the value of the third key... ')
+
+my_phone[key_name1] = value_name1
+my_phone[key_name2] = value_name2
+my_phone[key_name3] = value_name3
+
+print(my_phone)
